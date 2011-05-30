8a3942b1272d1000de9658dba037bd230f69e8ed
Garis <cuantico@gmail.com>
2011-05-30 09:40:10 -0700

    Fixing endreacher pieces

--- src/com/garis/generator/NumberGenerator.java
+++ src/com/garis/generator/NumberGenerator.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.garis.generator.chess.Cell;
 import com.garis.generator.chess.KeyPad;
+import com.garis.generator.chess.PieceFactory;
 import com.garis.generator.chess.pieces.Queen;
 
 public class NumberGenerator {
@@ -19,29 +20,30 @@ public class NumberGenerator {
 								{ 11, 0, 12} 
 				 			 };
 		KeyPad pad = new KeyPad(cellsArray);
-		pad.setDeepness(7);
+		pad.setDeepness(6);
 		pad.setMaxColumn(3);
 		pad.setMaxRow(4);
-		pad.setPiece(new Queen());
-		pad.setPrintDigits(false);
+		pad.setPiece(PieceFactory.buildQueen());
+		pad.setPrintDigits(true);
 		List<Cell> invalidKeys = new ArrayList<Cell>();
 		invalidKeys.add(new Cell(3,0));
 		invalidKeys.add(new Cell(3,2));
 		pad.setInvalidKeys(invalidKeys);
-		List<Cell> invalidStarting = new ArrayList<Cell>();
-		invalidStarting.add(new Cell(3,1));
-		invalidStarting.add(new Cell(0,0));
-		pad.setInvalidStartingKeys(invalidStarting);
-		
+		List<Cell> invalidStartingKeys = new ArrayList<Cell>();
+		invalidStartingKeys.add(new Cell(0,0));
+		invalidStartingKeys.add(new Cell(3,1));
+		pad.setInvalidStartingKeys(invalidStartingKeys);
 		try {
 			pad.setStartingCell(new Cell(0, 2));
 			pad.getWiredCells();
 			System.out
 					.println("Phone numbers that can be generated with the selected piece: "
 							+ pad.getMaxDigits());
+			System.out.println(pad.getDigits().toString());
 		} catch (InvalidPositionException e) {
 			e.printStackTrace();
 		}
+
 	}
 
 }
